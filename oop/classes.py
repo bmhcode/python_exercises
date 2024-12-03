cc9513ff497680d43b1b329ae885da560c00d78c
bmhcode <bmhwin@gmail.com>
2024-12-03 18:39:59 +0100

    property

--- oop/classes.py
+++ oop/classes.py
@@ -1,25 +1,34 @@
-class Employee:
-    def __init__(self, name, date_beirth, grade ):
-        self.name  = name
-        self.date_beirth = date_beirth
-        self.grade = grade
-    
-    def get_age(self):
-        return 2024 - self.date_beirth
-
-empl = Employee('Bouraghda Mohamed Elhadi', 1976, "Software Engeneer")
-print(empl.name)
-print(empl.get_age())
-
-list_employes = []
-while True:
-    name = input("Tap your name : ")
-    list_employes.append(name)
-    if name == "q" or name == "Q":
-        exit()
+class Personne:
+    def __init__(self, first_name, last_name, beirth_year):
+        self.first_name  = first_name
+        self.last_name  = last_name
+        self.beirth_year = beirth_year
+        # self.grade = grade
+        # self.notes = []
         
-for empl in list_employes:
-    print(empl,end='|')
-  
+    @property       
+    def full_name(self, first_name, last_name):
+        return f'{self.first_name} {last_name}'
+        
+    @full_name.setter        
+    def full_name(self, name):
+        self.last_name, self.last_name = name.split()
+           
+    @property 
+    def get_age(self):
+        return 2024 - self.beirth_year
+
+    def add_note(self):
+        for note in self.notes:
+            self.notes.append(note)
+            
+fname,lname = str(input("Tap your name : ")).split()
+year = int(input ('Enter your year of beirthday : '))
+   
+employe = Personne(fname,lname,year)
+# employe.full_name = "Bouraghda"
+print(employe.full_name(fname,lname)," | ",employe.get_age)
+   
+
         
         
